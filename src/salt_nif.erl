22d6d05ada17cf02f9f92a6ca2891c0b1770c17d
Jihyun Yu <yjh0502@gmail.com>
2015-10-22 23:01:33 +0900

    add linker flag, fix nif path

--- src/salt_nif.erl
+++ src/salt_nif.erl
@@ -43,11 +43,23 @@
 
 -on_load(init/0).
 
-%%%
+-define(APPNAME, salt).
+-define(LIBNAME, salt).
 
+%%%
 init() ->
-    Path = filename:join([code:priv_dir(salt), "salt_nif"]),
-    erlang:load_nif(Path, 0).
+    SoName = case code:priv_dir(?APPNAME) of
+        {error, bad_name} ->
+            case filelib:is_dir(filename:join(["..", priv])) of
+                true ->
+                    filename:join(["..", priv, ?LIBNAME]);
+                _ ->
+                    filename:join([priv, ?LIBNAME])
+            end;
+        Dir ->
+            filename:join(Dir, ?LIBNAME)
+    end,
+    erlang:load_nif(SoName, 0).
 
 %%% Exported from salt_nif.c.
 
